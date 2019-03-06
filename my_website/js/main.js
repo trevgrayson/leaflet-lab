d372f370225bd49450592755f71a2e829d55d74d
trevgrayson <34165870+trevgrayson@users.noreply.github.com>
2019-03-06 17:09:20 -0600

    Add files via upload
    activity 5b

--- my_website/js/main.js
+++ my_website/js/main.js
@@ -19,72 +19,212 @@ function createMap(){
     getData(map);
 };
 
-//added at Example 2.3 line 20...function to attach popups to each mapped feature
-function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-        }
-        layer.bindPopup(popupContent);
-    };
-};
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng, attributes){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = attributes[0];
 
-//Step 3: Add circle markers for point features to the map
-function createPropSymbols(data, map){
-    var attribute = "yr1995"
     //create marker options
-    var geojsonMarkerOptions = {
-        radius: 8,
-        fillColor: "#ff7800", 
+    var options = {
+        fillColor: "#ff4300",
         color: "#000",
         weight: 1,
         opacity: 1,
         fillOpacity: 0.8
     };
 
-    //calculate the radius of each proportional symbol
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string
+    var popupContent = "<p><b>Country:</b> " + feature.properties.Country + "</p>";
+    var year = attribute.split("yr")[1];
+    popupContent += "<p><b>Emmisions per capita in " + year + ":</b> " + feature.properties[attribute] + " tonnes   </p>";
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-options.radius)
+    });
+
+    //event listeners to open popup on hover
+    layer.on({
+        mouseover: function(){
+            this.openPopup();
+        },
+        mouseout: function(){
+            this.closePopup();
+        },
+        // click: function(){
+        //     $("#panel").html(popupContent);
+        // }
+    })
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//Add circle markers for point features to the map
+function createPropSymbols(data, map, attributes){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        },
+        // filter: filterSet(map, attributes, index)
+        //  function(feature, layer) {
+        //     // filterValue = emissionRange()
+        //     if (feature.properties.yr1995 > filterValue ) {
+        //         return 'true'
+        //     }
+        // }
+    }).addTo(map);
+};
+
+//calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
     //scale factor to adjust symbol size evenly
-    var scaleFactor = 50;
+    var scaleFactor = 20;
     //area based on attribute value and scale factor
     var area = attValue * scaleFactor;
     //radius calculated based on area
     var radius = Math.sqrt(area/Math.PI);
-    console.log(radius)
-    console.log("calcProp")
     return radius;
 };
-    //create a Leaflet GeoJSON layer and add it to the map
-    L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            //Step 5: For each feature, determine its value for the selected attribute
-            var attValue = Number(feature.properties[attribute]);
 
-            //Step 6: Give each feature's circle marker a radius based on its attribute value
-            geojsonMarkerOptions.radius = calcPropRadius(attValue);
-            console.log(geojsonMarkerOptions.radius)
+// //trying to create a function that returns a filter statement to the L.geojson
+// function filterSet(map, attributes, index){
+//     var filter = 1
+//     var indexVal = // this will be a multistep value probaby to split to filter by high, medium, and low per capita emissions
+//     filterText = "function(feature, layer, attr, indexVal) {if (feature.properties[attr] > indexVal) { return true}}"
+//     return filterText
+// };
 
-            //create circle markers
-            return L.circleMarker(latlng, geojsonMarkerOptions);
-        }
-    }).addTo(map);
+//Reference text from example. Trying to piece this with my method together
+// $('.menu-ui a').on('click', function() {
+//     // For each filter link, get the 'data-filter' attribute value.
+//     var filter = $(this).data('filter');
+//     $(this).addClass('active').siblings().removeClass('active');
+//     markers.setFilter(function(f) {
+//         // If the data-filter attribute is set to "all", return
+//         // all (true). Otherwise, filter on markers that have
+//         // a value set to true based on the filter name.
+//         return (filter === 'all') ? true : f.properties[filter] === true;
+//     });
+//     return false;
+// });
+
+//Create new sequence controls
+function createSequenceControls(map, attributes){
+    //create range input element (slider)
+    $('#panel').append('<button class="skip" id="reverse">Reverse</button>');
+    $('#reverse').html('<img src="img/reverse.png">');
+    $('#panel').append('<input class="range-slider" type="range">');
+    $('#panel').append('<button class="skip" id="forward">Skip</button>');
+    $('#forward').html('<img src="img/forward.png">');
+
+    //set slider attributes
+    $('.range-slider').attr({
+        max: 6,
+        min: 0,
+        value: 0,
+        step: 1
+    });
+
+    //Step 5: click listener for buttons
+    $('.skip').click(function(){
+        //get the old index value
+        var index = $('.range-slider').val();
+
+        //Step 6: increment or decrement depending on button clicked
+        if ($(this).attr('id') == 'forward'){
+            index++;
+            //Step 7: if past the last attribute, wrap around to first attribute
+            index = index > 6 ? 0 : index;
+        } else if ($(this).attr('id') == 'reverse'){
+            index--;
+            //Step 7: if past the first attribute, wrap around to last attribute
+            index = index < 0 ? 6 : index;
+        };
+
+        //update slider
+        $('.range-slider').val(index);
+        
+        //input listener for slider
+        $('.range-slider').on('input', function(){
+            //FINISH MANUAL SLIDER 
+        });
+        //pass new attribute to update symbols
+        updatePropSymbols(map, attributes[index]);
+    });
+    
 };
 
-//Step 2: Import GeoJSON data
+//Step 10: Resize proportional symbols according to new attribute values
+function updatePropSymbols(map, attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            //access feature properties
+            var props = layer.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+
+            //add city to popup content string
+            var popupContent = "<p><b>Country:</b> " + props.Country + "</p>";
+
+            //add formatted attribute to panel content string
+            console.log(attribute)
+            var year = attribute.split("yr")[1];
+            popupContent += "<p><b>Emmisions per capita in " + year + ":</b> " + props[attribute] + " tonnes   </p>";
+            
+            //replace the layer popup
+            layer.bindPopup(popupContent, {
+                offset: new L.Point(0,-radius)
+            });
+        };
+    });
+};
+
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (attribute.indexOf("yr") > -1){
+            attributes.push(attribute);
+        };
+    };
+
+    //check result
+    console.log(attributes);
+
+    return attributes;
+};
+
+//Import GeoJSON data
 function getData(map){
     //load the data
     $.ajax("data/countries.geojson", {
         dataType: "json",
         success: function(response){
-            //call function to create proportional symbols
-            createPropSymbols(response, map);
+            var attributes = processData(response);
+
+            createPropSymbols(response, map, attributes);
+            createSequenceControls(map, attributes);
+            // emissionRange(map, attributes)
         }
     });
 };
 
 $(document).ready(createMap);
-
-
